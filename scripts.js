9b04a16487d111589dde59bb2be64e7357761154
Andrew-lloyd Small <sandrewlsmall@gmail.com>
2019-11-01 02:40:00 -0500

    added right sidebar

--- scripts.js
+++ scripts.js
@@ -5,13 +5,17 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
  let d = new Date();
- document.getElementById("currentDate").innerHTML= dayNames[d.getDay()] + " " + [d.getDate()]
+ document.getElementById("currentDate").innerHTML= dayNames[d.getDay()] + " " + d.getDate()
   + " " + monthNames[d.getMonth()] + " " + d.getFullYear();
 }
 
-
-
-
+let clicked = 0;
+document.onclick = function(){
+    if (clicked === 0){
+    window.open("https://ourvle.mona.uwi.edu/", "Goolge");
+    clicked = 1;
+    }
+}
 
 
 
